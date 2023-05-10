beb2e2efba98912e263f3a209ed19b091ebc1a37
WMinusL <jebediahkerman@163.com>
2023-05-10 20:20:23 +0800

    hazard0

--- rtl/conv/NPUCore.v
+++ rtl/conv/NPUCore.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-
+//wyl
 module NPUCore # (
     parameter           MAC_IN_NUM              =   9                                    ,
     parameter           MAC_OUT_NUM             =   18                                   ,
