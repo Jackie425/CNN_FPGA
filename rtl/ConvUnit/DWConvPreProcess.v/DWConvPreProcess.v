b25bfb8784961224b55d67f5fb37bb0e2782996d
Jackie425 <jzj@emails.bjut.edu.cn>
2023-05-22 10:57:57 +0800

    new

--- rtl/ConvUnit/DWConvPreProcess.v/DWConvPreProcess.v
+++ rtl/ConvUnit/DWConvPreProcess.v/DWConvPreProcess.v
@@ -1,11 +1,55 @@
 `timescale 1ns / 1ps
 
 module DWConvPreProcess # (
-
+    parameter   DATA_WIDTH              =       8                           ,
+    parameter   IN_CHANNEL_NUM          =       9                           ,
+    parameter   OUT_CHANNEL_NUM         =       18                          ,
+    parameter   BUFF_LEN                =       320-2                       ,
+    parameter   DEPTH                   =       $clog2(BUFF_LEN)       
 )
-(
-    
+(           
+    input   wire                                                    clk             ,
+    input   wire                                                    rstn            ,
+//data path
+    input   wire    [OUT_CHANNEL_NUM*DATA_WIDTH-1:0]                data_in         ,
+    input   wire                                                    valid_in        ,
+    output  reg     [OUT_CHANNEL_NUM*IN_CHANNEL_NUM*DATA_WIDTH-1:0] win_reg         ,
+    output  wire                                                    valid_out       ,
+//control path
+    input  wire     [DEPTH-1:0]                                     buff_len_ctrl   ,
+    input  wire                                                     buff_len_rst    
 );
 
 
+    DWRowBuf # (
+      .DATA_WIDTH(DATA_WIDTH ),
+      .IN_CHANNEL_NUM(IN_CHANNEL_NUM ),
+      .OUT_CHANNEL_NUM(OUT_CHANNEL_NUM ),
+      .BUFF_LEN(BUFF_LEN ),
+      .DEPTH (DEPTH )
+    )
+    DWRowBuf_inst (
+      .clk          (clk          ),
+      .rstn         (rstn         ),
+      .data_in      (data_in      ),
+      .valid_in     (valid_in     ),
+      .data_out     (data_out     ),
+      .buff_len_ctrl(buff_len_ctrl),
+      .buff_len_rst ( buff_len_rst)
+    );
+
+    DWConvWinGen # (
+      .OUT_CHANNEL_NUM(OUT_CHANNEL_NUM ),
+      .DATA_WIDTH(DATA_WIDTH ),
+      .IN_CHANNEL_NUM (IN_CHANNEL_NUM )
+    )
+    DWConvWinGen_inst (
+      .clk (clk ),
+      .rstn (rstn ),
+      .data_in (data_in ),
+      .valid_in (valid_in ),
+      .win_reg (win_reg ),
+      .vailid_out  ( vailid_out)
+    );
+  
 endmodule
--- rtl/ConvUnit/DWConvPreProcess.v/DWRowBuf.v
+++ rtl/ConvUnit/DWConvPreProcess.v/DWRowBuf.v
@@ -8,20 +8,24 @@ module DWRowBuf # (
     parameter   DEPTH                   =       $clog2(BUFF_LEN)             
 )   
 (   
-    input  wire                                     clk             ,
-    input  wire                                     rstn            ,
+    input  wire                                         clk             ,
+    input  wire                                         rstn            ,
 //data path 
     input  wire     [OUT_CHANNEL_NUM*DATA_WIDTH-1:0]    data_in         ,
+    input  wire                                         valid_in        ,
     output wire     [OUT_CHANNEL_NUM*3*DATA_WIDTH-1:0]  data_out        ,
+    output wire                                         valid_out       ,
 //buffer control path
-    input  wire     [DEPTH-1:0]                     buff_len_ctrl   ,
-    input  wire                                     buff_len_rst    
+    input  wire     [DEPTH-1:0]                         buff_len_ctrl   ,
+    input  wire                                         buff_len_rst    
 );
 
-    reg     [DEPTH-1:0]     addr_in;
-    reg     [DEPTH-1:0]     addr_out;
+    reg     [DEPTH-1:0]                                     addr_in;
+    reg     [DEPTH-1:0]                                     addr_out;
     wire    [OUT_CHANNEL_NUM*IN_CHANNEL_NUM*DATA_WIDTH-1:0] win_reg;
-    
+    wire    [OUT_CHANNEL_NUM*DATA_WIDTH-1:0]                row1_data_out;
+    wire    [OUT_CHANNEL_NUM*DATA_WIDTH-1:0]                row2_data_out;
+
     always @(posedge clk or negedge rstn) begin
         if(!rstn) begin
             addr_in  <= BUFF_LEN - 1'b1;
@@ -41,19 +45,33 @@ module DWRowBuf # (
         end
     end
 
-    DWConvWinGen 
-    #(
-      .OUT_CHANNEL_NUM(OUT_CHANNEL_NUM ),
-      .DATA_WIDTH(DATA_WIDTH ),
-      .IN_CHANNEL_NUM (IN_CHANNEL_NUM )
-    )
-    DWConvWinGen_inst (
-      .clk (clk ),
-      .rstn (rstn ),
-      .data_in (data_in ),
-      .valid_in (valid_in ),
-      .win_out (win_out ),
-      .vailid_out  ( vailid_out)
+    DRM_144_512 DRM_144_512_row1_inst (
+        .wr_data(data_in),        // input [143:0]
+        .wr_addr(addr_in),        // input [8:0]
+        .wr_en(valid_in),            // input
+        .wr_clk(clk),          // input
+        .wr_clk_en(1'b1),    // input
+        .wr_rst(~rstn),          // input
+        .rd_addr(addr_out),        // input [8:0]
+        .rd_data(row1_data_out),        // output [143:0]
+        .rd_clk(clk),          // input
+        .rd_clk_en(1'b1),    // input
+        .rd_rst(~rstn)           // input
     );
-  
+
+    DRM_144_512 DRM_144_512_row2_inst (
+        .wr_data(row1_data_out),        // input [143:0]
+        .wr_addr(addr_in),        // input [8:0]
+        .wr_en(valid_in),            // input
+        .wr_clk(clk),          // input
+        .wr_clk_en(1'b1),    // input
+        .wr_rst(~rstn),          // input
+        .rd_addr(addr_out),        // input [8:0]
+        .rd_data(row2_data_out),        // output [143:0]
+        .rd_clk(clk),          // input
+        .rd_clk_en(1'b1),    // input
+        .rd_rst(~rstn)           // input
+    );
+
+      assign data_out = {row2_data_out,row1_data_out,data_in};
 endmodule
